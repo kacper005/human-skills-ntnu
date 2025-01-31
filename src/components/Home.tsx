a8b125e426483a8885914dae6c472ad9612a4233
sam <sjackson864@outlook.com>
2025-01-31 18:44:16 +0100

    removed quizzes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import GameCardGrid from "./GameCardGrid";
-import Quizzes from "./Quizzes";
 
 const Home: React.FC = () => {
   return (
     <div>
       <GameCardGrid />
-      <div style={{ margin: "40px 0" }}></div>
-      <Quizzes />
     </div>
   );
 };
