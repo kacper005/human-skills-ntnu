b937498404f3484dd54d09be35bc61c139466e64
skcajs <sjackson864@outlook.com>
2025-02-07 12:30:34 +0100

    test main lock

--- src/components/GameCardGrid.tsx
+++ src/components/GameCardGrid.tsx
@@ -21,14 +21,14 @@ const gameData: Game[] = [
     type: "attention",
     difficulty: "easy",
     image: "/game_images/attention.png",
-    descriptionFull: `This is an Attention Game: it records your ability to focus (pay
+    descriptionFull: `This is an Attention Game: It records your ability to focus (pay
           attention) to the right things (ignore everything else).
           \n\nIndicate the direction of the space shuttle in the middle (ignore all
           others - if any). You respond by using "left" or "right" arrow on your
           keyboard (or using buttons on the screen).
           \n\nThe game will last for 1 minute; score will be calculated based on
           number of answers and number of correct answers i.e. a combination of
-          speed and accuracy.`
+          speed and accuracy.`,
   },
   {
     id: 2,
@@ -38,7 +38,7 @@ const gameData: Game[] = [
     type: "attention",
     difficulty: "easy",
     image: "/game_images/balloon.png",
-    descriptionFull: `This is a Risk Game: it records your willingness to take risk for a higher potential reward. \n\nMaximize your points collection by inflateing the balloon to its limit - you inflate by pressing "Inflate Balloon" and you collect your gains by pressing "$$$ Collected"\n\nThe more the balloon is inflated the more you collect, but at the risk of the balloon bursting (and you loose your money); the inflation burst point of the balloon changes randomly.\n\nThe goal is to collect as much points ($$$) as possible over 10 atempts - there is a 1 minute time cap.`
+    descriptionFull: `This is a Risk Game: it records your willingness to take risk for a higher potential reward. \n\nMaximize your points collection by inflateing the balloon to its limit - you inflate by pressing "Inflate Balloon" and you collect your gains by pressing "$$$ Collected"\n\nThe more the balloon is inflated the more you collect, but at the risk of the balloon bursting (and you loose your money); the inflation burst point of the balloon changes randomly.\n\nThe goal is to collect as much points ($$$) as possible over 10 atempts - there is a 1 minute time cap.`,
   },
   {
     id: 3,
@@ -47,7 +47,7 @@ const gameData: Game[] = [
     type: "problem-solving",
     difficulty: "easy",
     image: "/game_images/cogflex.png",
-    descriptionFull: `This is a Cognitive Flexibility Game: it records your ability to detect, apply and adapt to change.\n\nPress on the the correct card option (out of 4 at the top) that matches the master card (bottom section) - following the identified pattern (color of items, number of items or shape of items); pattern will at irregular intervals change.\n\nThe game will last for 2 minutes; score will be calculated based on number of answers and number of correct answers i.e. a combination of speed and accuracy.\n\n`
+    descriptionFull: `This is a Cognitive Flexibility Game: it records your ability to detect, apply and adapt to change.\n\nPress on the the correct card option (out of 4 at the top) that matches the master card (bottom section) - following the identified pattern (color of items, number of items or shape of items); pattern will at irregular intervals change.\n\nThe game will last for 2 minutes; score will be calculated based on number of answers and number of correct answers i.e. a combination of speed and accuracy.\n\n`,
   },
   {
     id: 4,
@@ -56,7 +56,7 @@ const gameData: Game[] = [
     type: "critical thingking",
     difficulty: "easy",
     image: "/game_images/troll.jpg",
-    descriptionFull: `This is a TestyMcTestFace Game: it records your ability to test, mcFace and face the TestFace.\n\nPress on the the correct face option (out of 4 at the top) that matches the McTestFace - following the identified McFace (color of the Test, number of Testy's MCface or shape of McFace); pattern will at irregular intervals change.\n\nThe game will last for 2 minutes; score will be calculated based on number of answers and number of correct answers i.e. a combination of speed and accuracy.\n\n`
+    descriptionFull: `This is a TestyMcTestFace Game: it records your ability to test, mcFace and face the TestFace.\n\nPress on the the correct face option (out of 4 at the top) that matches the McTestFace - following the identified McFace (color of the Test, number of Testy's MCface or shape of McFace); pattern will at irregular intervals change.\n\nThe game will last for 2 minutes; score will be calculated based on number of answers and number of correct answers i.e. a combination of speed and accuracy.\n\n`,
   },
 
   {
@@ -66,7 +66,7 @@ const gameData: Game[] = [
     type: "critical thingking",
     difficulty: "easy",
     image: "/game_images/troll.jpg",
-    descriptionFull: `SHOOTYMCFACE\n\n`
+    descriptionFull: `SHOOTYMCFACE\n\n`,
   },
 ];
 
