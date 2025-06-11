7dbdded34fb3911764a7eb85ab56f227a994b7e3
Kacper <kacperm@stud.ntnu.no>
2025-06-11 17:35:51 +0200

    Fixed AdminRoute

--- src/context/AdminRoute.tsx
+++ src/context/AdminRoute.tsx
@@ -1,10 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { Role } from "../enums/Role";
 
 export const AdminRoute = () => {
-  const { isLoggedIn, user } = useAuth();
+  const { isLoggedIn, user, loading } = useAuth();
 
-  if (!isLoggedIn || user?.role !== "ADMIN") {
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isLoggedIn || user?.role !== Role.ADMIN) {
     return <Navigate to="/not-found" />;
   }
 
